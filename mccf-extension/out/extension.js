26b06f4619323528218d7abb8fd503ca113511c0
Vincenzo Mauro <vimauro@microsoft.com>
2023-06-16 17:11:42 +0100

    wip

--- mccf-extension/out/extension.js
+++ mccf-extension/out/extension.js
@@ -3,19 +3,44 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
 const vscode = require("vscode");
 const Docker = require("dockerode");
+const path = require("path");
+const fs = require("fs");
 function activate(context) {
     const docker = new Docker();
     let disposable = vscode.commands.registerCommand('extension.createDockerContainer', async () => {
-        const imageName = await vscode.window.showQuickPick(['3.0.10', '3.0.11', '4.0.0', '4.0.3'], {
+        const ccfVersion = await vscode.window.showQuickPick(['3.0.10', '3.0.11', '4.0.0', '4.0.3'], {
             placeHolder: 'Enter the version of CCF to be used',
             canPickMany: false,
             ignoreFocusOut: true
         });
-        const container = await docker.createContainer({
-            Image: `mcr.microsoft.com/ccf/app/run-js:${imageName}-virtual`
+        const dockerfilePath = path.join(__dirname, 'dockerfile');
+        const contextPath = path.join(__dirname, '.');
+        // List all files in the current directory
+        fs.readdir(contextPath, (err, files) => {
+            if (err) {
+                vscode.window.showErrorMessage(`Error reading directory: ${err}`);
+                return;
+            }
+            console.log(files);
         });
+        const stream = await docker.buildImage({
+            context: contextPath,
+            src: [dockerfilePath],
+        }, {
+            buildargs: { 'CCF_VERSION': `${ccfVersion}`, },
+        });
+        stream.setEncoding('utf8');
+        stream.on('data', (chunk) => {
+            vscode.window.createOutputChannel('Docker Build Output').appendLine(chunk);
+        });
+        /*const container = await docker.createContainer({
+            Image: `mcr.microsoft.com/ccf/app/run-js:${ccfVersion}-virtual`,
+            name: 'ccf',
+        });
+
         await container.start();
-        vscode.window.showInformationMessage(`Container ${container.id} created and started`);
+
+        vscode.window.showInformationMessage(`Container ${container.id} created and started`);  */
     });
     context.subscriptions.push(disposable);
 }
--- mccf-extension/src/extension.ts
+++ mccf-extension/src/extension.ts
@@ -1,24 +1,53 @@
 import * as vscode from 'vscode';
 import * as Docker from 'dockerode';
+import path = require('path');
+import fs = require('fs');
 
 export function activate(context: vscode.ExtensionContext) {
     const docker = new Docker();
 
     let disposable = vscode.commands.registerCommand('extension.createDockerContainer', async () => {
 
-        const imageName = await vscode.window.showQuickPick(['3.0.10', '3.0.11', '4.0.0', '4.0.3'], {
+        const ccfVersion = await vscode.window.showQuickPick(['3.0.10', '3.0.11', '4.0.0', '4.0.3'], {
             placeHolder: 'Enter the version of CCF to be used',
             canPickMany: false,
             ignoreFocusOut: true
         });
 
-        const container = await docker.createContainer({
-            Image: `mcr.microsoft.com/ccf/app/run-js:${imageName}-virtual`
+        const dockerfilePath = path.join(__dirname, 'dockerfile');
+        const contextPath = path.join(__dirname, '.');
+
+        // List all files in the current directory
+        fs.readdir(contextPath, (err, files) => {
+            if (err) {
+                vscode.window.showErrorMessage(`Error reading directory: ${err}`);
+                return;
+            }
+            console.log(files);
         });
 
-        await container.start();
+        const stream = await docker.buildImage(
+            {
+                context: contextPath,
+                src: [dockerfilePath],
+            },
+            {
+                buildargs: { 'CCF_VERSION': `${ccfVersion}`, },
+            });
 
-        vscode.window.showInformationMessage(`Container ${container.id} created and started`);
+        stream.setEncoding('utf8');
+        stream.on('data', (chunk) => {
+            vscode.window.createOutputChannel('Docker Build Output').appendLine(chunk);
+        }); 
+
+         /*const container = await docker.createContainer({
+             Image: `mcr.microsoft.com/ccf/app/run-js:${ccfVersion}-virtual`,
+             name: 'ccf',
+         }); 
+ 
+         await container.start();
+ 
+         vscode.window.showInformationMessage(`Container ${container.id} created and started`);  */
     });
 
     context.subscriptions.push(disposable);
