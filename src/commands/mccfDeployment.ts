08a1c247d53eb522da9c4bcc4fa54f6fad829e70
sjanney9 <t-sjanney@microsoft.com>
2023-07-05 10:08:44 -0700

    added azure cli check

--- src/commands/mccfDeployment.ts
+++ src/commands/mccfDeployment.ts
@@ -3,38 +3,16 @@ const { ResourceManagementClient } = require("@azure/arm-resources");
 const readlineSync = require('readline-sync');
 
 export async function createMCCFInstance() {
-    // Prompt the user for input
-    const subscriptionId = readlineSync.question("Enter the subscription ID: ");
-    const resourceGroupName = readlineSync.question("Enter the resource group name: ");
-    const location = readlineSync.question("Enter the location: ");
-    const mccfInstanceName = readlineSync.question("Enter the MCCF instance name: ");
+    try {
+        //Select the specific fiel 
 
-    const credential = new DefaultAzureCredential();
-    const resourceClient = new ResourceManagementClient(credential, subscriptionId);
 
-    // Define the MCCF instance parameters
-    const mccfInstanceParams = {
-        location: location,
-        sku: {
-            name: "Standard"
-        },
-        properties: {
-            parentId: "/",
-            tenantId: "<your-tenant-id>"
-        }
-    };
 
-    // Create the MCCF instance
-    const mccfInstance = await resourceClient.resources.beginCreateOrUpdate(
-        resourceGroupName,
-        "Microsoft.Management/managementGroupSubscriptions",
-        mccfInstanceName,
-        mccfInstanceParams
-    ).awaitCompletion();  // Wait for the operation to complete
 
-    console.log("MCCF instance created:", mccfInstance);
-}
+    }catch (error) {
 
+    }
 createMCCFInstance().catch((error) => {
     console.error("Error occurred:", error);
 });
+}
