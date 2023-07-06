faee35d2112ae4f921a408903011eea455e2a39b
sarah-oli-condelli <t-scondelli@microsoft.com>
2023-07-06 09:12:09 -0700

    Function Organization

--- src/commands/addMember.ts
+++ src/commands/addMember.ts
@@ -1,40 +1,52 @@
 import * as vscode from "vscode";
-import { exec, execSync } from 'child_process'; 
+import { exec, execSync } from "child_process";
 import path = require("path");
-const fs = require('fs');
+const fs = require("fs");
 
-export async function addMember(context: vscode.ExtensionContext){
-
-    try {
-        // Prompt user to enter the member name
-        const memberName = await vscode.window.showInputBox({
-            prompt: "Enter the member name",
-            placeHolder: "Member name"
-        });
-    
-        // If no member name is entered, report it to the user
-        if (!memberName || memberName.length === 0) {
-            vscode.window.showInformationMessage("No member name entered");
-            return;
-        }
-    
-        // Create a certificate directory folder in the current environment where we will store the member certificates
+export async function addMember(context: vscode.ExtensionContext) {
+    // Create a certificate directory folder in the current environment where we will store the member certificates
+    function createFolder() {
         const folderName = "Certificates";
         const currentPath = path.join(process.cwd(), folderName);
-        if (!fs.existsSync(currentPath)){
+        if (!fs.existsSync(currentPath)) {
             fs.mkdirSync(currentPath);
         }
 
-        // Enter the folder 
-        process.chdir(folderName);
-
-        // Run the script using the user input as a parameter
-        const addMember = execSync("bash " + context.extensionPath + "/src/scripts/addMember.sh --name " + memberName + " 2>&1");
-   
-    } catch (error) {
-        console.error(error);
-        // show the error
-        vscode.window.showErrorMessage("Error adding member");
+        // Enter current folder
+        process.chdir(currentPath);
     }
 
+    // Prompt user to enter member name
+    async function memberGenerator(memberName: string) {
+        try {
+            // Generate the member certificates using the keygenerator.sh script
+            const generateMember = execSync(
+                "bash " + context.extensionPath + "/src/scripts/generateMember.sh --name " + memberName + " 2>&1"
+            );
+
+            // Show the output of the script
+            vscode.window.showInformationMessage(generateMember.toString());
+        } catch (error) {
+            console.error(error);
+            vscode.window.showErrorMessage("Error adding member");
+        }
+    }
+
+    // Prompt user to enter member name
+    const memberName = await vscode.window.showInputBox({
+        prompt: "Enter the member name",
+        placeHolder: "Member name",
+    });
+
+    // If no member name is entered, report it to the user
+    if (!memberName || memberName.length === 0) {
+        vscode.window.showInformationMessage("No member name entered");
+        return;
+    }
+
+    // Call the createFolder function
+    createFolder();
+
+    // Call the memberGenerator function
+    memberGenerator(memberName);
 }
