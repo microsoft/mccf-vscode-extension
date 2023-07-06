8f74fed6693a1f5f7e93cfd06229d69be5876e21
sjanney9 <t-sjanney@microsoft.com>
2023-07-06 09:10:28 -0700

    changes to mccf deployment command

--- src/commands/mccfDeployment.ts
+++ src/commands/mccfDeployment.ts
@@ -4,7 +4,6 @@ const {exec} = require("child_process");
 
 export async function createMCCFInstance() {
     try {
-<<<<<<< HEAD
         exec('az --version', (error: any, stdout: string, stderr: any) => {
             if (error) {
                 console.log(error);
@@ -37,17 +36,4 @@ export async function createMCCFInstance() {
     const resourceGroup = await window.showInputBox({ prompt: 'Enter the resource group you want this instance to be placed' });
 
     execSync(`az confidentialledger managedccfs create --members "[{certificate:${certificateDir},identifier:${identifier}}]"--name ${names} --resource-group ${resourceGroup}`);
-=======
-        //Select the specific fiel 
-
-
-
-
-    }catch (error) {
-
-    }
-createMCCFInstance().catch((error) => {
-    console.error("Error occurred:", error);
-});
->>>>>>> 08a1c247d53eb522da9c4bcc4fa54f6fad829e70
 }
