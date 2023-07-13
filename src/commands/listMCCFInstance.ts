37b822f82fb1555eb6d6dcf00157ea43d991349d
sjanney9 <t-sjanney@microsoft.com>
2023-07-13 10:20:52 -0700

    test git push

--- src/commands/listMCCFInstance.ts
+++ src/commands/listMCCFInstance.ts
@@ -1,5 +1,7 @@
 import * as vscode from "vscode";
 import { execSync } from "child_process";
+import { window } from "vscode";
+
 
 export async function listMCCFInstances() {
   const resourceGroup = await vscode.window.showInputBox({
@@ -11,14 +13,44 @@ export async function listMCCFInstances() {
     );
     return;
   }
+  const message =  execSync(`az resource list --resource-group ${resourceGroup} --output table --only-show-errors`);
   try {
-    execSync(`az resource list --resource-group ${resourceGroup} --output table --only-show-errors`);
+    message 
   } catch (error) {
     vscode.window.showErrorMessage('An error occurred while retrieving the resources. Please enter a valid resource group and try again');
   }
 
+  interface Subscription {
+    name: string;
+    id: string;
+  }
+
+  // Retrieve a list of subscriptions
+  const subscriptionsOutput = execSync("az account list --output json").toString();
+  const subscriptions: Subscription[] = JSON.parse(subscriptionsOutput);
+
+  // Convert the subscriptions to QuickPick items
+  const subscriptionItems = subscriptions.map((subscription) => ({
+    label: subscription.name,
+    description: subscription.id,
+  }));
+
+  // Let the user choose a subscription using QuickPick
+  const selectedSubscription = await window.showQuickPick(subscriptionItems, {
+    placeHolder: "Select a subscription",
+    ignoreFocusOut: true,
+  });
+
+  if (!selectedSubscription) {
+    vscode.window.showErrorMessage("Please select a subscription");
+    return;
+  }
+
+  const subscriptionId = selectedSubscription.description;
+ 
+ 
 //command is ran in the terminal
-const command = `az confidentialledger managedccfs list --subscription 027da7f8-2fc6-46d4-9be9-560706b60fec --resource-group ${resourceGroup} --only-show-errors --query "[].name" -o tsv`;
+const command = `az confidentialledger managedccfs list --subscription ${subscriptionId} --resource-group ${resourceGroup} --only-show-errors --query "[].name" -o tsv`;
   let output = execSync(command).toString();
   if(output === "") {
     vscode.window.showErrorMessage('No instances found. Please enter a valid resource group and try again');
