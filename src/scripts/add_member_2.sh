72e1755ef5ab5867db66a1cfd0e5802a8811fbcf
sarah-oli-condelli <t-scondelli@microsoft.com>
2023-07-17 10:01:26 -0700

    Script Updates - SetUserFile

--- src/scripts/add_member_2.sh
+++ src/scripts/add_member_2.sh
@@ -8,12 +8,12 @@ source common_utils.sh
 function create_member_proposal {
   local certFile=$1
   local keyFile=$2
-  local setUserFile="${id}_set_user.json"
+  local setUserFile="set_${id}.json"
 
   cert=$(< "$certFile" sed '$!G' | paste -sd '\\n' -)
   key=$(< "$keyFile" sed '$!G' | paste -sd '\\n' -)
 
-  cat <<JSON > "$setUserFile"
+  cat <<JSON > $setUserFile
 {
   "actions": [
   {
