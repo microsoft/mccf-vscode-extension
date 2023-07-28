5eaf96db025ac3b56237830b0c89125bf80d9225
Andrea Piccione <apiccione@microsoft.com>
2023-07-28 12:09:29 +0100

    Update vote command

--- src/commands/voteProposal.ts
+++ src/commands/voteProposal.ts
@@ -2,82 +2,105 @@ import * as vscode from "vscode";
 import * as utilities from "../Utilities/osUtilities";
 import { runCommandInTerminal } from "../Utilities/extensionUtils";
 import { execSync } from "child_process";
+import { logAndDisplayError, logAndThrowError } from "../Utilities/errorUtils";
+import path = require("path");
+
+const voteQuickPickItems: vscode.QuickPickItem[] = [
+  {
+    label: "Accept",
+  },
+  {
+    label: "Reject",
+  },
+  {
+    label: "Custom",
+  },
+];
 
 export async function voteProposal(specialContext: vscode.ExtensionContext) {
-  // Prompt user to enter network url
-  const networkUrl = await vscode.window.showInputBox({
-    ignoreFocusOut: true,
-    prompt: "Enter the CCF network URL",
-    placeHolder: "https://example.confidential-ledger.azure.com",
-  });
+  try {
+    // Prompt user to enter network url
+    const networkUrl = await vscode.window.showInputBox({
+      ignoreFocusOut: true,
+      prompt: "Enter the CCF network URL",
+      placeHolder: "https://example.confidential-ledger.azure.com",
+    });
 
-  // If no network url is entered, report it to the user
-  if (!networkUrl || networkUrl.length === 0) {
-    vscode.window.showInformationMessage("No network url entered");
-    return;
+    // If no network url is entered, report it to the user
+    if (!networkUrl || networkUrl.length === 0) {
+      vscode.window.showInformationMessage("No network url entered");
+      return;
+    }
+
+    // Prompt user to select an active proposal
+    const proposalId = await getProposal(networkUrl);
+
+    // If no proposal id is selected, report it to the user
+    if (!proposalId || proposalId.length === 0) {
+      return;
+    }
+
+    // Prompt user to cast a vote
+    const vote = await castVote(specialContext);
+
+    // If no vote is selected, report it to the user
+    if (!vote || vote.length === 0) {
+      vscode.window.showInformationMessage("No vote selected");
+      return;
+    }
+
+    // Prompt user to enter signing cert via file explorer
+    const signingCert = await vscode.window.showOpenDialog({
+      canSelectFiles: true,
+      canSelectFolders: false,
+      canSelectMany: false,
+      openLabel: "Select signing certificate",
+      title: "Select signing certificate",
+      filters: { "PEM files": ["pem"] },
+    });
+
+    // Check if signing cert is undefined
+    if (!signingCert) {
+      vscode.window.showInformationMessage("No signing cert selected");
+      return;
+    }
+
+    // Prompt user to enter signing key via file explorer
+    const signingKey = await vscode.window.showOpenDialog({
+      canSelectFiles: true,
+      canSelectFolders: false,
+      canSelectMany: false,
+      openLabel: "Select signing key",
+      title: "Select signing key",
+      filters: { "PEM files": ["pem"] },
+    });
+
+    // Check if signing key is undefined
+    if (!signingKey) {
+      vscode.window.showInformationMessage("No signing key selected");
+      return;
+    }
+
+    // Retrieve paths of sign cert, sign key, and voting file
+    const signingCertPath = utilities.getPathOSAgnostic(signingCert[0].fsPath);
+    const signingKeyPath = utilities.getPathOSAgnostic(signingKey[0].fsPath);
+    const votingFilePath = utilities.getPathOSAgnostic(vote);
+
+    // Call the vote proposal function
+    voteForProposal(
+      networkUrl,
+      signingCertPath,
+      signingKeyPath,
+      proposalId,
+      votingFilePath,
+      specialContext.extensionPath,
+    );
+
+    // Display message to the user
+    vscode.window.showInformationMessage("Vote submission in progress");
+  } catch (error: any) {
+    logAndDisplayError("Error when voting for a proposal", error);
   }
-
-  // Prompt user to select an active proposal
-  const proposalId = await getProposal(networkUrl);
-
-  // If no proposal id is selected, report it to the user
-  if (!proposalId || proposalId.length === 0) {
-    return;
-  }
-
-  // Prompt user to cast a vote
-  const vote = await castVote(specialContext);
-
-  // If no vote is selected, report it to the user
-  if (!vote || vote.length === 0) {
-    vscode.window.showInformationMessage("No vote selected");
-    return;
-  }
-
-  // Prompt user to enter signing cert via file explorer
-  const signingCert = await vscode.window.showOpenDialog({
-    canSelectFiles: true,
-    canSelectFolders: false,
-    canSelectMany: false,
-    openLabel: "Select signing certificate",
-    filters: { "Cert.pem files": ["pem"] },
-  });
-
-  // Check if signing cert is undefined
-  if (!signingCert) {
-    vscode.window.showInformationMessage("No signing cert selected");
-    return;
-  }
-
-  // Prompt user to enter signing key via file explorer
-  const signingKey = await vscode.window.showOpenDialog({
-    canSelectFiles: true,
-    canSelectFolders: false,
-    canSelectMany: false,
-    openLabel: "Select signing key",
-    filters: { "Privk.pem files": ["pem"] },
-  });
-
-  // Check if signing key is undefined
-  if (!signingKey) {
-    vscode.window.showInformationMessage("No signing key selected");
-    return;
-  }
-
-  // Retrieve paths of sign cert, sign key, and voting file
-  const signingCertPath = utilities.getPathOSAgnostic(signingCert[0].fsPath);
-  const signingKeyPath = utilities.getPathOSAgnostic(signingKey[0].fsPath);
-  const votingFilePath = utilities.getPathOSAgnostic(vote);
-
-  // Call the vote proposal function
-  voteForProposal(
-    networkUrl,
-    signingCertPath,
-    signingKeyPath,
-    proposalId,
-    votingFilePath,
-    specialContext.extensionPath,
-  );
 }
 
 // Function that runs the vote_proposal.sh script to vote on a proposal
@@ -90,12 +113,10 @@ async function voteForProposal(
   extensionPath: string,
 ) {
   try {
-    const command = `cd ${extensionPath}/dist; ${utilities.getBashCommand()} ./vote_proposal.sh --network-url ${networkUrl} --signing-cert ${signingCert} --signing-key ${signingKey} --proposal-id ${proposalId} --vote-file ${voteFile}`;
+    const command = `cd ${extensionPath}/dist/scripts; ${utilities.getBashCommand()} ./vote_proposal.sh --network-url ${networkUrl} --signing-cert ${signingCert} --signing-key ${signingKey} --proposal-id ${proposalId} --vote-file ${voteFile}`;
     runCommandInTerminal("Proposal Voting", command);
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      vscode.window.showErrorMessage(error.message);
-    }
+  } catch (error: any) {
+    logAndThrowError("Error when submitting the vote", error);
   }
 }
 
@@ -137,13 +158,13 @@ async function getProposal(networkUrl: string): Promise<string> {
       proposalQuickPickItems,
       {
         ignoreFocusOut: true,
-        placeHolder: "Select a ballot",
+        placeHolder: "Select a proposal",
       },
     );
 
     // Check if no proposal was selected
     if (!selectedProposal) {
-      vscode.window.showInformationMessage("No ballot selected");
+      vscode.window.showInformationMessage("No proposal selected");
       return "";
     }
 
@@ -162,18 +183,6 @@ export async function castVote(
   extContext: vscode.ExtensionContext,
 ): Promise<string> {
   // Display a quick pick menu with vote accept or vote reject options
-  const voteQuickPickItems: vscode.QuickPickItem[] = [
-    {
-      label: "Accept",
-    },
-    {
-      label: "Reject",
-    },
-    {
-      label: "Custom",
-    },
-  ];
-
   const selectedVote = await vscode.window.showQuickPick(voteQuickPickItems, {
     ignoreFocusOut: true,
     placeHolder: "Select a vote",
@@ -187,10 +196,10 @@ export async function castVote(
   // Return the appropriate voting file
   if (selectedVote.label === "Accept") {
     // return path of accept vote file
-    return extContext + "/src/scripts/vote_accept.json";
+    return path.join(extContext.extensionPath, "dist/votes/vote_accept.json");
   } else if (selectedVote.label === "Reject") {
     // return path of reject vote file
-    return extContext + "/src/scripts/vote_reject.json";
+    return path.join(extContext.extensionPath, "dist/votes/vote_reject.json");
   } else {
     // Prompt user to enter custom voting file via file explorer
     const customVote = await vscode.window.showOpenDialog({
@@ -198,6 +207,7 @@ export async function castVote(
       canSelectFolders: false,
       canSelectMany: false,
       openLabel: "Select custom voting file",
+      title: "Select custom voting file",
       filters: { "JSON files": ["json"] },
     });
 
