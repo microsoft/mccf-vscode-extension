1ee87045cb71b822e0b86e50d7c4d275a73ea9ad
sarah-oli-condelli <t-scondelli@microsoft.com>
2023-08-10 11:00:09 -0700

    Azure SDK utilities commands

--- src/Utilities/azureSDKUtilities.ts
+++ src/Utilities/azureSDKUtilities.ts
@@ -2,8 +2,16 @@ const {
   ClientSecretCredential,
   DefaultAzureCredential,
 } = require("@azure/identity");
+import {
+  ConfidentialLedgerClient,
+  Ledger,
+} from "@azure/arm-confidentialledger";
 const { SubscriptionClient } = require("@azure/arm-subscriptions");
 require("dotenv").config();
+import exp = require("constants");
+import { ResourceGroup } from "@azure/arm-resources";
+import { ResourceManagementClient } from "@azure/arm-resources";
+import { QuickPickItem, window } from "vscode";
 
 let credentials: any;
 
@@ -24,43 +32,64 @@ if (process.env.NODE_ENV && process.env.NODE_ENV === "production") {
   }
 }
 
-async function listSubscriptions() {
-  try {
-    // use credential to authenticate with Azure SDKs
-    const client = new SubscriptionClient(credentials);
+// List the subscriptions for the authenticated account
+async function listSubscriptions(
+  subscriptionClient: typeof SubscriptionClient,
+) {
+  // get details of each subscription
+  const subscriptions = await subscriptionClient.subscriptions.list();
 
-    // get details of each subscription
-    for await (const item of client.subscriptions.list()) {
-      const subscriptionDetails = await client.subscriptions.get(
-        item.subscriptionId,
-      );
-      /* 
-          Each item looks like:
-        
-          {
-            id: '/subscriptions/123456',
-            subscriptionId: '123456',
-            displayName: 'YOUR-SUBSCRIPTION-NAME',
-            state: 'Enabled',
-            subscriptionPolicies: {
-              locationPlacementId: 'Internal_2014-09-01',
-              quotaId: 'Internal_2014-09-01',
-              spendingLimit: 'Off'
-            },
-            authorizationSource: 'RoleBased'
-          },
-      */
-      console.log(subscriptionDetails);
-    }
-  } catch (err) {
-    console.error(JSON.stringify(err));
-  }
+  // create an array of QuickPickItems from the subscription details
+  const subscriptionItems: QuickPickItem[] = subscriptions.map(
+    (subscription: any) => {
+      return {
+        label: subscription.displayName!,
+        description: subscription.subscriptionId!,
+      };
+    },
+  );
+
+  // display the QuickPick menu and return the selected subscription ID
+  const selectedSubscription = await window.showQuickPick(subscriptionItems, {
+    placeHolder: "Select a subscription",
+  });
+  return selectedSubscription;
 }
 
-listSubscriptions()
-  .then(() => {
-    console.log("done");
-  })
-  .catch((ex) => {
-    console.log(ex);
-  });
+// List the resource groups in the selected subscription
+export async function listResourceGroups(): Promise<string | undefined> {
+  try {
+    // use credential to authenticate with Azure SDKs
+    const credential = new DefaultAzureCredential();
+    const subscriptionClient = new SubscriptionClient(credential);
+    const subscription = await listSubscriptions(subscriptionClient);
+
+    // create a new instance of the ResourceManagementClient class
+    const client = new ResourceManagementClient(
+      credential,
+      subscription.subscriptionId,
+    );
+
+    // retrieve a list of resource groups
+    const resourceGroups = await client.resourceGroups.list();
+
+    // create an array of QuickPickItems from the resource group details
+    const items: QuickPickItem[] = resourceGroups.map(
+      (group: ResourceGroup) => {
+        return {
+          label: group.name!,
+          description: group.id!,
+        };
+      },
+    );
+
+    // display the QuickPick menu and return the selected resource group ID
+    const selectedGroup = await window.showQuickPick(items, {
+      placeHolder: "Select a resource group",
+    });
+    return selectedGroup?.description;
+  } catch (error) {
+    console.error(error);
+    window.showErrorMessage("Failed to list resource groups");
+  }
+}
