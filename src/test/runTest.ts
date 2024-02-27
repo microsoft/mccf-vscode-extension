1785668233bcaf256eed6c10b8bb2ce90f825d88
weirenlai <weirenlai@microsoft.com>
2024-02-27 14:22:16 -0800

    add verbose for debugging

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -13,7 +13,11 @@ async function main() {
     const extensionTestsPath = path.resolve(__dirname, "./suite/index");
 
     // Download VS Code, unzip it and run the integration test
-    await runTests({ extensionDevelopmentPath, extensionTestsPath });
+    await runTests({
+      extensionDevelopmentPath,
+      extensionTestsPath,
+      launchArgs: ["--verbose"],
+    });
   } catch (err) {
     console.error("Failed to run tests", err);
     process.exit(1);
